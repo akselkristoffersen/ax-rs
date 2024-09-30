b1b5e0073d05a7d3dab384d64c3951035c9d8dbd
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-09-30 15:53:05 +0200

    Improve packet_iterator tests

--- src/packet_iterator.rs
+++ src/packet_iterator.rs
@@ -36,51 +36,75 @@ impl<'a, T, F> Iterator for PacketIterator<'a, T, F> where
 mod tests {
     use super::*;
 
-    #[test]
-    fn simple() {
-        let protocol = |slice: &[i32]| -> usize {
-            if slice.is_empty() {
-                return 0
-            }
-            slice[0] as usize
-        };
-
-        {
-            let buffer = vec![2, 0, 4, 1, 0, 0, 3, 0, 8];
-            let mut it = PacketIterator::new(&buffer, protocol);
-            assert_eq!(it.next(), Some(&buffer[0..2]));
-            assert_eq!(it.next(), Some(&buffer[2..6]));
-            assert_eq!(it.next(), Some(&buffer[6..9]));
-            assert_eq!(it.next(), None);
-            assert_eq!(it.next(), None);
+    fn protocol(buf: &[i32]) -> usize {
+        if buf.is_empty() {
+            return 0
         }
-
-        {
-            let buffer = vec![2, 0, 4, 1, 0, 0, 3, 0, 8, 3];
-            let mut it = PacketIterator::new(&buffer, protocol);
-            assert_eq!(it.next(), Some(&buffer[0..2]));
-            assert_eq!(it.next(), Some(&buffer[2..6]));
-            assert_eq!(it.next(), Some(&buffer[6..9]));
-            assert_eq!(it.next(), None);
-            assert_eq!(it.next(), None);
-        }
-        
-        {
-            let buffer = vec![2, 0, 4, 1, 0, 0, 3, 0, 8, 3, 0];
-            let mut it = PacketIterator::new(&buffer, protocol);
-            assert_eq!(it.next(), Some(&buffer[0..2]));
-            assert_eq!(it.next(), Some(&buffer[2..6]));
-            assert_eq!(it.next(), Some(&buffer[6..9]));
-            assert_eq!(it.next(), None);
-            assert_eq!(it.next(), None);
-        }
-
-        {
-            let buffer = vec![1, 0, 4, 1, 0, 0, 3, 0, 8, 3, 0];
-            let mut it = PacketIterator::new(&buffer, protocol);
-            assert_eq!(it.next(), Some(&buffer[0..1]));
-            assert_eq!(it.next(), None);
-            assert_eq!(it.next(), None);
-        }
+        buf[0] as usize
     }
+
+    #[test]
+    fn empty_buffer() {
+        let buffer = vec![];
+        let mut it = PacketIterator::new(&buffer, protocol);
+        assert_eq!(it.next(), None);
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn complete_packets() {
+        let buffer = vec![2, 0, 4, 1, 0, 0, 3, 0, 8];
+        let mut it = PacketIterator::new(&buffer, protocol);
+        assert_eq!(it.next(), Some(&buffer[0..2]));
+        assert_eq!(it.next(), Some(&buffer[2..6]));
+        assert_eq!(it.next(), Some(&buffer[6..9]));
+        assert_eq!(it.next(), None);
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn complete_packets_with_lambda() {
+        let protocol = |buf: &[i32]| -> usize {
+            protocol(buf)
+        };
+        let buffer = vec![2, 0, 4, 1, 0, 0, 3, 0, 8];
+        let mut it = PacketIterator::new(&buffer, protocol);
+        assert_eq!(it.next(), Some(&buffer[0..2]));
+        assert_eq!(it.next(), Some(&buffer[2..6]));
+        assert_eq!(it.next(), Some(&buffer[6..9]));
+        assert_eq!(it.next(), None);
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn ignore_last_packet_which_is_incomplete1() {
+        let buffer = vec![2, 0, 4, 1, 0, 0, 3, 0, 8, 3];
+        let mut it = PacketIterator::new(&buffer, protocol);
+        assert_eq!(it.next(), Some(&buffer[0..2]));
+        assert_eq!(it.next(), Some(&buffer[2..6]));
+        assert_eq!(it.next(), Some(&buffer[6..9]));
+        assert_eq!(it.next(), None);
+        assert_eq!(it.next(), None);
+    }
+        
+    #[test]
+    fn ignore_last_packet_which_is_incomplete2() {
+        let buffer = vec![2, 0, 4, 1, 0, 0, 3, 0, 8, 3, 0];
+        let mut it = PacketIterator::new(&buffer, protocol);
+        assert_eq!(it.next(), Some(&buffer[0..2]));
+        assert_eq!(it.next(), Some(&buffer[2..6]));
+        assert_eq!(it.next(), Some(&buffer[6..9]));
+        assert_eq!(it.next(), None);
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn second_packet_is_corrupt() {
+        let buffer = vec![1, 0, 4, 1, 0, 0, 3, 0, 8, 3, 0];
+        let mut it = PacketIterator::new(&buffer, protocol);
+        assert_eq!(it.next(), Some(&buffer[0..1]));
+        assert_eq!(it.next(), None);
+        assert_eq!(it.next(), None);
+    }
+    
 }
